658c9d219d0bf6715ad2992c4583e0ceafd29e23
RaresCodreanu <rares961@yahoo.com>
2023-02-15 12:18:08 +0200

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,7 +47,7 @@
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-0">
-                        Log in or register to continue
+                        Log in or register to continue.
                     </div>
                 </div>
 
